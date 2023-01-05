f3a5d5d24129a5eb02dfd7ce8fc524f2e3e2ec2d
Hiroki Chiyoda <contact@arafipro.com>
2023-01-05 16:22:18 +0900

    色の定数ファイルを作成

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// https://mycolor.space/?hex=%230C8874&sub=1
+const Color appBarColor = Color(0xFF0C8874);
+const Color buttonColor = Color(0xFF00A989);
+const Color bodyColor = Color(0xFFE7FFF8);
+const Color textColor = Color(0xFFF3EADA);

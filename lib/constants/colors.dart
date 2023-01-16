e0d01aaee84e73edb4c7966bac7f519cd7db2e68
Hiroki Chiyoda <contact@arafipro.com>
2023-01-16 16:23:31 +0900

    アプリのスタイルを変更
    編集ページの入力の色と太さを変更

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:jpstockminimemo/constants/imports.dart';
 
 // https://mycolor.space/?hex=%230C8874&sub=1
-const Color appBarColor = Color(0xFF0C8874);
+const Color appBarColor = Color(0xFF0B836F);
 const Color bgColor = Color(0xFFEBFDF8);
 const Color buttonColor = Color(0xFF00A989);
 const Color textColor = Color(0xFFFFFFFF);
